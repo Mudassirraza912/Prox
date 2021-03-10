1a3942543cbc1c5e7146eaaf30f2128e0d21deec
Mudassir Raza <razamudassir912@gmail.com>
2021-03-11 00:38:35 +0500

    filter ux

--- src/components/SearchFilter/index.js
+++ src/components/SearchFilter/index.js
@@ -1,12 +1,24 @@
 import React, { createRef, forwardRef } from "react";
-import { View, Text, TouchableOpacity, StyleSheet} from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet, Platform} from 'react-native'
 import ActionSheet from "react-native-actions-sheet";
 import { fontStyles } from "../../constants/fontStyles";
 import Ionicons from 'react-native-vector-icons/Ionicons'
+import MultiSlider from "@ptomasroos/react-native-multi-slider";
+import CustomMarker from "./CustomMarker";
+import CustomLabel from "./CustomLabel";
+import Chips from '../Chips/index'
+
+export const SearchFilter = forwardRef(({
+    onClose = () => ref.current?.setModalVisible(),
+}, ref) => {
 
 
-export const SearchFilter = forwardRef((props, ref) => {
-
+    const [
+        nonCollidingMultiSliderValue,
+        setNonCollidingMultiSliderValue,
+      ] = React.useState([0, 100]);
+      
+      nonCollidingMultiSliderValuesChange = values => setNonCollidingMultiSliderValue(values);
     return(
         <ActionSheet ref={ref}>
             <View style={[styles.container]}>
@@ -14,9 +26,75 @@ export const SearchFilter = forwardRef((props, ref) => {
                         <Text style={fontStyles.ProximaSemiBold}>
                             Filter Your Search
                         </Text>
-                        <View>
-                            <Ionicons name="cross" />
+                        <TouchableOpacity activeOpacity={.6} onPress={onClose} style={[styles.closeContainerStyle]}>
+                            <Ionicons color="#fff" name="close-outline" size={25} />
+                        </TouchableOpacity>
+                    </View>
+                    <View style={[styles.blockContainer]}>
+                        <Text style={fontStyles.ProximaRegularP2}>Distance</Text>
+                        <View style={[styles.itemContainer]}>
+                            <MultiSlider
+                                values={[
+                                nonCollidingMultiSliderValue[0],
+                                nonCollidingMultiSliderValue[1],
+                                ]}
+                                selectedStyle={{backgroundColor:'#FFBE00'}}
+                                unselectedStyle={{backgroundColor:'#F1F2FA'}}
+                                sliderLength={280}
+                                onValuesChange={nonCollidingMultiSliderValuesChange}
+                                min={0}
+                                max={100}
+                                step={1}
+                                allowOverlap={false}
+                                snapped
+                                minMarkerOverlapDistance={40}
+                                customMarker={CustomMarker}
+                                customLabel={CustomLabel}
+                                enableLabel
+                            />
                         </View>
+                    </View>
+                    <View style={[styles.blockContainer]}>
+                        <Text style={fontStyles.ProximaRegularP2}>Price Ranges</Text>
+                        <View style={[styles.itemContainer]}>
+                            <MultiSlider
+                                values={[
+                                nonCollidingMultiSliderValue[0],
+                                nonCollidingMultiSliderValue[1],
+                                ]}
+                                selectedStyle={{backgroundColor:'#FFBE00'}}
+                                unselectedStyle={{backgroundColor:'#F1F2FA'}}
+                                sliderLength={280}
+                                onValuesChange={nonCollidingMultiSliderValuesChange}
+                                min={0}
+                                max={100}
+                                step={1}
+                                allowOverlap={false}
+                                snapped
+                                minMarkerOverlapDistance={40}
+                                customMarker={CustomMarker}
+                                customLabel={CustomLabel}
+                                enableLabel
+                            />
+                        </View>
+                    </View>
+                    <View style={[styles.blockContainer]}>
+                        <Text style={fontStyles.ProximaRegularP2}>Tags</Text>
+                        <Chips
+                                data={[
+                                    "McDonald's",
+                                    "Pizza",
+                                    "Burger King",
+                                    "Chicken wings",
+                                    "Burger",
+                                    "Pasta",
+                                    "Greenwich",
+                                    "Coffee",
+                                    "Steak",
+                                ]}
+                                chipTextStyle={{ color: "#6A7C92" }}
+
+                            />
                     </View>
                 </View>
         </ActionSheet>
@@ -27,6 +105,7 @@ export const SearchFilter = forwardRef((props, ref) => {
 const styles = StyleSheet.create({
     container: {
         // flex: 1,
+        paddingVertical: Platform.OS == "ios" ? 20 : 10
     },
     blockContainer: {
         paddingVertical: 5,
@@ -38,5 +117,10 @@ const styles = StyleSheet.create({
     rowSpaceBtw : {
         flexDirection: 'row',
         justifyContent: 'space-between'
+    },
+    closeContainerStyle: {
+        bottom: 5,
+        backgroundColor: '#6A7C92', 
+        borderRadius: 20
     }
 })
