489d9bf47c0e9e68b56685b08d30cee47c6299b7
muhammadharis1 <mh7365496@gmail.com>
2021-03-13 01:39:19 +0500

    paralax fixed

--- src/components/PrallaxTabs/PrallaxTab.js
+++ src/components/PrallaxTabs/PrallaxTab.js
@@ -3,10 +3,15 @@ import { Animated, Dimensions, Platform, Text, TouchableOpacity, View } from "re
 import { Body, Header, List, ListItem as Item, ScrollableTab, Tab, TabHeading, Tabs, Title } from "native-base";
 import { HorizontalCards } from "../Cards/HorizontalCards";
 import { DEFAULT_THEME_COLOR } from "../../constants/colors";
+import { fontStyles } from "../../constants/fontStyles";
+import Button from "../Button";
+import { ItemCard } from "../Cards/ItemCard";
+
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 const IMAGE_HEIGHT = 250;
-const HEADER_HEIGHT = Platform.OS === "ios" ? 100 : 50;
+const HEADER_HEIGHT = Platform.OS === "ios" ? 60 : 50;
 const SCROLL_HEIGHT = IMAGE_HEIGHT - HEADER_HEIGHT;
 const THEME_COLOR = DEFAULT_THEME_COLOR;
 const FADED_THEME_COLOR = "rgba(85,186,255, 0.8)";
@@ -46,17 +51,23 @@ export class ParallaxDemo extends Component {
     inputRange: [0, IMAGE_HEIGHT - HEADER_HEIGHT],
     outputRange: ["rgba(255,255,255,0)", "rgba(255,255,255,1)"]
   });
+
+  trnaslateX = this.scroll.interpolate({
+    inputRange: [0, IMAGE_HEIGHT- 5],
+    outputRange: [0, 40],
+    extrapolate: "clamp"
+  })
   tabContent = (x, i) => <View style={{ height: this.state.height }}>
     <List onLayout={({ nativeEvent: { layout: { height } } }) => {
       this.heights[i] = height;
       if (this.state.activeTab === i) this.setState({ height })
     }}>
-      {new Array(x).fill(null).map((_, i) => <HorizontalCards containerStyle={{ margin: 10 }} />)}
+      {new Array(x).fill(null).map((_, i) => <ItemCard containerStyle={{ margin: 10 }} />)}
     </List></View>;
   heights = [500, 500];
   state = {
     activeTab: 0,
-    height: 600
+    height: 1000
   };
 
   constructor(props) {
@@ -77,13 +88,17 @@ export class ParallaxDemo extends Component {
             zIndex: 1
           }}>
           <Header transparent style={{ backgroundColor: 'transparent' }} hasTabs>
-            <Body>
-              <Title>
+            {/* <Body> */}
+              {/* <Title>
                 <Animated.Text style={{ color: this.textColor, fontWeight: "bold" }}>
                   Tab Parallax
               </Animated.Text>
-              </Title>
-            </Body>
+              </Title> */}
+
+              <TouchableOpacity onPress={() => alert()} style={{ backgroundColor: "rgba(0,0,0,.36)", height: 30, width: 30, borderRadius: 100, alignItems: 'center', justifyContent: 'center', position: 'absolute', left: 10, top: Platform.OS == "ios"  ? 10 : 55 }}>
+                <Ionicons name="chevron-back" size={25} color={"#fff"} />
+              </TouchableOpacity>
+            {/* </Body> */}
           </Header>
         </Animated.View>
         <Animated.ScrollView
@@ -99,57 +114,85 @@ export class ParallaxDemo extends Component {
               source={require('../../assets/images/bg.png')}
               style={{ height: IMAGE_HEIGHT, width: "100%", opacity: this.imgOpacity }} />
           </Animated.View>
-          <View style={{backgroundColor: '#fff', borderTopLeftRadius: 20, borderTopRightRadius: 20, height: 40, top: -10}} /> 
+
           <Tabs
-            prerenderingSiblingsNumber={3}
+            prerenderingSiblingsNumber={0}
+            tabBarBackgroundColor="trnsparent"
             onChangeTab={({ i }) => {
               this.setState({ height: this.heights[i], activeTab: i })
             }}
             renderTabBar={(props) => {
-             return (
+              return (
                 <>
-                    <Animated.View
-                style={{ transform: [{ translateY: this.tabY }], zIndex: 1, width: "100%", backgroundColor: "white" }}>
-                <ScrollableTab
-                  {...props}
-                  renderTab={(name, page, active, onPress, onLayout) => (
-                    <TouchableOpacity key={page}
-                      onPress={() => onPress(page)}
-                      onLayout={onLayout}
-                      activeOpacity={0.4}>
-                      <Animated.View
-                        style={{
-                          flex: 1,
-                          height: 100,
-                          backgroundColor: this.tabBg
-                        }}>
-                        <TabHeading scrollable
-                          style={{
-                            backgroundColor: "#F1F2FA",
-                            width: SCREEN_WIDTH / 2
-                          }}
-                          active={active}>
-                          <Animated.Text style={{
-                            fontWeight: active ? "bold" : "normal",
-                            color: this.textColor,
-                            fontSize: 14
-                          }}>
-                            {name}
-                          </Animated.Text>
-                        </TabHeading>
+                  <Animated.View
+                    style={{ transform: [{ translateY: this.tabY }], zIndex: 1, width: "100%", backgroundColor: "#F1F2FA" }}>
+                    <Animated.View style={{ backgroundColor: '#fff', borderTopLeftRadius: 20, borderTopRightRadius: 20, padding: 10, top: -10 }}>
+                      <Animated.Text style={[fontStyles.ProximaBoldH1, { transform: [{ translateX: this.trnaslateX }] }]}>McDonald's</Animated.Text>
+                      <Animated.Text style={[fontStyles.ProximaRegularP2, { fontSize: 12, color: "#6A7C92", paddingVertical: 10 }]}>102  Trouser Leg Road, Springfield</Animated.Text>
+
+                      <Animated.View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
+                        <Animated.View style={{ flexDirection: "row" }}>
+                          <Animated.Text>5 miles </Animated.Text>
+                          <Animated.Text> . </Animated.Text>
+                          <Animated.Text> 4.8 </Animated.Text>
+
+                        </Animated.View>
+                        <Button
+                          title={"Check Reviews"}
+                          backgroundColor={"#FFBE00"}
+                          containerStyle={{ width: "30%", borderRadius: 50, height: undefined, paddingVertical: 10 }}
+                          textColor="#000"
+                        />
                       </Animated.View>
-                    </TouchableOpacity>
-                  )}
-                  underlineStyle={{ backgroundColor: this.textColor }} />
-              </Animated.View>
+                    </Animated.View>
+                    <ScrollableTab
+                      {...props}
+                      renderTab={(name, page, active, onPress, onLayout) => (
+                        <TouchableOpacity key={page}
+                          onPress={() => onPress(page)}
+                          onLayout={onLayout}
+                          activeOpacity={0.4}>
+                          <Animated.View
+                            style={{
+                              flex: 1,
+                              height: 100,
+                              backgroundColor: this.tabBg
+                            }}>
+                            <TabHeading scrollable
+                              style={{
+                                backgroundColor: "#F1F2FA",
+                                height: "100%",
+                              
+                                // width: SCREEN_WIDTH / 2
+                              }}
+                              active={active}>
+                              <Animated.Text style={[fontStyles.ProximaRegularP2,{
+                                fontWeight: active ? "bold" : "normal",
+                                color: active ? this.textColor : "#6A7C92",
+                                fontSize: 12
+                              }]}>
+                                {name}
+                              </Animated.Text>
+                            </TabHeading>
+                          </Animated.View>
+                        </TouchableOpacity>
+                      )}
+                      underlineStyle={{ backgroundColor: this.textColor }} />
+                  </Animated.View>
                 </>
               )
             }}
           >
-            <Tab  heading="Tab 1">
-              {this.tabContent(30, 0)}
+            <Tab heading="All">
+              {this.tabContent(15, 0)}
             </Tab>
-            <Tab heading="Tab 2">
+            <Tab heading="Burgers">
+              {this.tabContent(15, 1)}
+            </Tab>
+            <Tab heading="Fries">
+              {this.tabContent(15, 0)}
+            </Tab>
+            <Tab heading="Chicken">
               {this.tabContent(15, 1)}
             </Tab>
           </Tabs>
