8e0cd80630a831f0ec624bd246928d8903467e40
muhammadharis1 <mh7365496@gmail.com>
2021-04-18 00:29:13 +0500

    lgin done

--- src/screens/Signin/signin.js
+++ src/screens/Signin/signin.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, StyleSheet, Platform, ScrollView, TouchableOpacity } from 'react-native'
 import Button from '../../components/Button'
 import { HorizontalCards } from '../../components/Cards/HorizontalCards'
@@ -7,17 +7,57 @@ import Input from '../../components/Input'
 import { NotificationSettingList } from '../../components/List/notificationSettingList'
 import { DEFAULT_THEME_COLOR } from '../../constants/colors'
 import { fontStyles } from '../../constants/fontStyles'
+import RenderError from '../../utils/renderError'
+import { userLogin } from '../../stores/actions/user.action'
+import { connect } from 'react-redux'
 
+const SignIn = ({ navigation, userLogin }) => {
 
-const SignIn = ({navigation}) => {
+    const [username, setUsername] = useState("")
+    const [usernameError, setUsernameError] = useState("")
+    const [password, setPassword] = useState("")
+    const [passwordError, setPasswordError] = useState("")
 
+    const validate = () => {
+        let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+        setUsernameError(username ? "" : "Username is required")
+        setPasswordError(password ? "" : "Password is required")
+
+        if (username && password) {
+            return true
+
+        } else {
+            return false
+        }
+    }
+
+    const handleSubmit = async () => {
+        let formStatus = validate()
+
+        if (formStatus) {
+            const user = {
+                "username": username,
+                "password": password,
+            }
+            let { status } = await userLogin(user)
+            if (status) {
+                navigation.navigate("EnableLocation")
+            }
+
+            // alert("done")
+        } else {
+            // alert("errors")
+            validate()
+        }
+
+    }
 
     return (
         <View style={styles.mainContainer}>
             <ScrollView contentContainerStyle={{ paddingBottom: 20 }}>
                 <View style={{ flex: 1 }}>
-        
-                    <View style={[styles.blockContainer, {marginTop: Platform.OS == "android" ? 0 : 50}]}>
+
+                    <View style={[styles.blockContainer, { marginTop: Platform.OS == "android" ? 0 : 50 }]}>
                         <View style={styles.itemContainer}>
                             <Text style={fontStyles.ProximaBoldH1}>Welcome back </Text>
                         </View>
@@ -27,22 +67,24 @@ const SignIn = ({navigation}) => {
                     </View>
 
                     <View style={styles.blockContainer}>
-                         <Input label="Enter Email" keyboardType="default"/>
+                        <Input label="Enter Username" value={username} onChangeText={setUsername} keyboardType="default" />
+                        <RenderError errorText={usernameError} />
                     </View>
                     <View style={styles.blockContainer}>
-                        <Input label="Password" isPassword keyboardType="default" />
+                        <Input label="Password" isPassword value={password} onChangeText={setPassword} keyboardType="default" />
+                        <RenderError errorText={passwordError} />
                     </View>
 
-            <View style={styles.blockContainer}>
-                <Button onPress={() => navigation.navigate("ForgotPassword")} title="Forgot Password?" titleStyle={[fontStyles.ProximaRegularP2, {color:'#000'}]} backgroundColor="transparent" />
-            </View>
+                    <View style={styles.blockContainer}>
+                        <Button onPress={() => navigation.navigate("ForgotPassword")} title="Forgot Password?" titleStyle={[fontStyles.ProximaRegularP2, { color: '#000' }]} backgroundColor="transparent" />
+                    </View>
 
                     <View style={styles.blockContainer}>
-                        <Button 
-                            onPress={() => navigation.navigate("EnableLocation")}
-                            title="Sign in" 
-                            titleStyle={fontStyles.ProximaSemiBold} 
-                            />
+                        <Button
+                            onPress={() => handleSubmit()}
+                            title="Sign in"
+                            titleStyle={fontStyles.ProximaSemiBold}
+                        />
                     </View>
 
                     <View style={styles.signUpText}>
@@ -56,13 +98,13 @@ const SignIn = ({navigation}) => {
                         <Button
                             onPress={() => { }}
                             title={"Connect with Facebook"}
-                            titleStyle={[fontStyles.ProximaSemiBold, {  }]}  
+                            titleStyle={[fontStyles.ProximaSemiBold, {}]}
                             type="facebook"
                         />
                     </View>
                     <View style={styles.blockContainer}>
                         <Button
-                            onPress={() => {}}
+                            onPress={() => { }}
                             title={"Connect with Google"}
                             titleStyle={[fontStyles.ProximaSemiBold]}
                             type={"google"}
@@ -98,4 +140,14 @@ const styles = StyleSheet.create({
     }
 
 })
-export default SignIn;
+
+const mapStateToProps = (state) => ({
+
+})
+
+const mapDispatchToProps = {
+    userLogin
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn);
